25691891fb0088d8deff279f05c50ae1e25f9d5f
michalkurka <michalk@h2o.ai>
2020-12-17 21:05:54 +0200

    Added python tests
    (cherry picked from commit 271a03610f38fd21649d2219ae40624e57ed9ef4)

--- h2o-py/h2o/h2o.py
+++ h2o-py/h2o/h2o.py
@@ -155,7 +155,7 @@ def version_check():
 
 def init(url=None, ip=None, port=None, name=None, https=None, cacert=None, insecure=None, username=None, password=None,
          cookies=None, proxy=None, start_h2o=True, nthreads=-1, ice_root=None, log_dir=None, log_level=None,
-         max_log_file_size=None, enable_assertions=True, max_mem_size=None, min_mem_size=None, strict_version_check=None, 
+         max_log_file_size=None, enable_assertions=True, max_mem_size=None, min_mem_size=None, strict_version_check=None,
          ignore_config=False, extra_classpath=None, jvm_custom_args=None, bind_to_localhost=True, **kwargs):
     """
     Attempt to connect to a local server, or if not successful start a new server and connect to it.
@@ -475,14 +475,12 @@ def import_file(path=None, destination_frame=None, parse=True, header=0, sep=Non
                                         skipped_columns, custom_non_data_line_markers, partition_by, quotechar)
 
 
-def load_grid(grid_file_path, load_params_references=False):
+def load_grid(grid_file_path):
     """
     Loads previously saved grid with all its models from the same folder
 
     :param grid_file_path: A string containing the path to the file with grid saved.
      Grid models are expected to be in the same folder.
-    :param load_params_references: If true will attemt to reload saved objects referenced by grid parameters
-      (e.g. training frame, calibration frame), will fail if grid was saved without referenced objects.
 
     :return: An instance of H2OGridSearch
 
@@ -513,21 +511,18 @@ def load_grid(grid_file_path, load_params_references=False):
     """
 
     assert_is_type(grid_file_path, str)
-    response = api(
-        "POST /3/Grid.bin/import", 
-        {"grid_path": grid_file_path, "load_params_references": load_params_references}
-    )
+    response = api("POST /3/Grid.bin/import", {"grid_path": grid_file_path})
     return get_grid(response["name"])
 
 
-def save_grid(grid_directory, grid_id, save_params_references=False):
+def save_grid(grid_directory, grid_id, export_cv_predictions=False):
     """
     Export a Grid and it's all its models into the given folder
 
     :param grid_directory: A string containing the path to the folder for the grid to be saved to.
     :param grid_id: A character string with identification of the Grid in H2O.
-    :param save_params_references: True if objects referenced by grid parameters
-      (e.g. training frame, calibration frame) should also be saved. 
+    :param export_cv_prediction: A boolean flag indicating whether the models exported from the grid should be saved
+        with CV Holdout Frame predictions. 
 
     :examples:
 
@@ -556,12 +551,10 @@ def save_grid(grid_directory, grid_id, save_params_references=False):
     """
     assert_is_type(grid_directory, str)
     assert_is_type(grid_id, str)
-    api(
-        "POST /3/Grid.bin/" + grid_id + "/export", 
-        {"grid_directory": grid_directory, "save_params_references": save_params_references}
-    )
+    assert_is_type(export_cv_predictions, bool)
+    params = {"grid_directory": grid_directory, "export_cv_predictions": export_cv_predictions}
+    api("POST /3/Grid.bin/" + grid_id + "/export", params)
     return grid_directory + "/" + grid_id
-
 
 def import_hive_table(database=None, table=None, partitions=None, allow_multi_format=False):
     """
@@ -600,7 +593,7 @@ def import_hive_table(database=None, table=None, partitions=None, allow_multi_fo
     return get_frame(j.dest_key)
 
 
-def import_sql_table(connection_url, table, username, password, columns=None, optimize=True, 
+def import_sql_table(connection_url, table, username, password, columns=None, optimize=True,
                      fetch_mode=None, num_chunks_hint=None):
     """
     Import SQL table to H2OFrame in memory.
@@ -1414,7 +1407,7 @@ def download_all_logs(dirname=".", filename=None, container=None):
     return api("GET /3/Logs/download%s" % type, save_to=save_to)
 
 
-def save_model(model, path="", force=False):
+def save_model(model, path="", force=False, export_cv_predictions=False):
     """
     Save an H2O Model object to disk. (Note that ensemble binary models can now be saved using this method.)
     The owner of the file saved is the user by which H2O cluster was executed.
@@ -1422,6 +1415,8 @@ def save_model(model, path="", force=False):
     :param model: The model object to save.
     :param path: a path to save the model at (hdfs, s3, local)
     :param force: if True overwrite destination directory in case it exists, or throw exception if set to False.
+    :param export_cv_predictions: logical, indicates whether the exported model
+        artifact should also include CV Holdout Frame predictions
 
     :returns: the path of the saved model
 
@@ -1438,17 +1433,21 @@ def save_model(model, path="", force=False):
     assert_is_type(model, ModelBase)
     assert_is_type(path, str)
     assert_is_type(force, bool)
+    assert_is_type(export_cv_predictions, bool)
     path = os.path.join(os.getcwd() if path == "" else path, model.model_id)
-    return api("GET /99/Models.bin/%s" % model.model_id, data={"dir": path, "force": force})["dir"]
+    data = {"dir": path, "force": force, "export_cv_predictions": export_cv_predictions}
+    return api("GET /99/Models.bin/%s" % model.model_id, data=data)["dir"]
 
 
-def download_model(model, path=""):
+def download_model(model, path="", export_cv_predictions=False):
     """
     Download an H2O Model object to the machine this python session is currently connected to.
     The owner of the file saved is the user by which python session was executed.
 
     :param model: The model object to download.
     :param path: a path to the directory where the model should be saved.
+    :param export_cv_predictions: logical, indicates whether the exported model
+        artifact should also include CV Holdout Frame predictions
 
     :returns: the path of the downloaded model
 
@@ -1460,12 +1459,16 @@ def download_model(model, path=""):
     >>> my_model.train(y = "CAPSULE",
     ...                x = ["AGE", "RACE", "PSA", "GLEASON"],
     ...                training_frame = h2o_df)
-    >>> h2o.download_model(my_model, path='', force=True)
+    >>> h2o.download_model(my_model, path='')
     """
     assert_is_type(model, ModelBase)
     assert_is_type(path, str)
+    assert_is_type(export_cv_predictions, bool)
     path = os.path.join(os.getcwd() if path == "" else path, model.model_id)
-    return api("GET /3/Models.fetch.bin/%s" % model.model_id, save_to=path)
+    return api("GET /3/Models.fetch.bin/%s" % model.model_id,
+               data={"export_cv_predictions": export_cv_predictions},
+               save_to=path)
+
 
 def upload_model(path):
     """
@@ -1550,34 +1553,10 @@ def export_file(frame, path, force=False, sep=",", compression=None, parts=1, he
     assert_is_type(compression, str, None)
     assert_is_type(header, bool)
     assert_is_type(quote_header, bool)
-    H2OJob(api("POST /3/Frames/%s/export" % (frame.frame_id), 
-               data={"path": path, "num_parts": parts, "force": force, 
+    H2OJob(api("POST /3/Frames/%s/export" % (frame.frame_id),
+               data={"path": path, "num_parts": parts, "force": force,
                      "compression": compression, "separator": ord(sep),
                      "header": header, "quote_header": quote_header}), "Export File").poll()
-
-
-def load_frame(frame_id, path, force=True):
-    """
-    Load frame previously stored in H2O's native format.
-
-    This will load a data frame from file-system location. Stored data can be loaded only with a cluster of the same
-    size and same version the the one which wrote the data. The provided directory must be accessible from all nodes 
-    (HDFS, NFS). Provided frame_id must be the same as the one used when writing the data.
-    
-    :param frame_id: the frame ID of the original frame
-    :param path: a filesystem location where to look for frame data
-    :param force: overwrite an already existing frame (defaults to true)
-    :returns: A Frame object.
-    
-    :examples:
-    
-    >>> iris = h2o.load_frame("iris_weather.hex", "hdfs://namenode/h2o_data")
-    """
-    H2OJob(api(
-        "POST /3/Frames/load",
-        data={"frame_id": frame_id, "dir": path, "force": force}
-    ), "Load frame data").poll()
-    return get_frame(frame_id)
 
 
 def cluster():
@@ -1945,7 +1924,7 @@ def make_metrics(predicted, actual, domain=None, distribution=None, weights=None
     params = {"domain": domain, "distribution": distribution}
     if weights is not None:
         params["weights_frame"] = weights.frame_id
-    params["auc_type"] = auc_type    
+    params["auc_type"] = auc_type
     res = api("POST /3/ModelMetrics/predictions_frame/%s/actuals_frame/%s" % (predicted.frame_id, actual.frame_id),
               data=params)
     return res["model_metrics"]
@@ -2019,7 +1998,7 @@ def _default_custom_distribution_source_provider(obj):
         return _inspect_methods_separately(obj)
     else:
         return _default_source_provider(obj)
-    
+
 
 def upload_custom_metric(func, func_file="metrics.py", func_name=None, class_name=None, source_provider=None):
     """
@@ -2268,7 +2247,7 @@ def print_mojo(mojo_path, format="json", tree_index=None):
     >>> mojo_path = gbm_h2o.download_mojo()
     >>> mojo_str = h2o.print_mojo(mojo_path)
     >>> mojo_dict = json.loads(mojo_str)
-    """    
+    """
     assert_is_type(mojo_path, str)
     assert_is_type(format, str, None)
     assert_satisfies(format, format in [None, "json", "dot"])
@@ -2295,85 +2274,6 @@ def print_mojo(mojo_path, format="json", tree_index=None):
         return output
     else:
         raise H2OError("Unable to print MOJO: %s" % output)
-
-def estimate_cluster_mem(ncols, nrows, num_cols = 0, string_cols = 0, cat_cols = 0, time_cols = 0, uuid_cols = 0):
-    """
-    Computes an estimate for cluster memory usage in GB.
-    
-    Number of columns and number of rows are required. For a better estimate you can provide a counts of different
-    types of columns in the dataset.
-
-    :param ncols: total number of columns in a dataset. An required parameter, integer, can't be negative
-    :param nrows: total number of rows in a dataset. An required parameter, integer, can't be negative 
-    :param num_cols: number of numeric columns in a dataset. Integer, can't be negative.
-    :param string_cols: number of string columns in a dataset. Integer, can't be negative.
-    :param cat_cols: number of categorical columns in a dataset. Integer, can't be negative.
-    :param time_cols: number of time columns in a dataset. Integer, can't be negative.
-    :param uuid_cols: number of uuid columns in a dataset. Integer, can't be negative.
-    :return: An memory estimate in GB.
-
-    :example:
-
-    >>> from h2o import estimate_cluster_mem
-    >>> ### I will load an parquet file with 18 columns and 2 million lines
-    >>> estimate_cluster_mem(18, 2000000)
-    >>> ### I will load an other parquet file with 16 columns and 2 million lines, I ask for a more precise estimate 
-    >>> ### because I know 12 of 16 columns are categorical and one of 16 columns consist of uuids.
-    >>> estimate_cluster_mem(18, 2000000, cat_cols=12, uuid_cols=1)
-    >>> ### I will load an parquet file with 8 columns and 31 million lines, I ask for a more precise estimate 
-    >>> ### because I know 4 of 8 columns are categorical and 4 of 8 columns consist of numbers.
-    >>> estimate_cluster_mem(ncols=8, nrows=31000000, cat_cols=4, num_cols=4)
-    
-    """    
-    import math
-    
-    if (ncols < 0):
-        raise ValueError("ncols can't be a negative number")
-    
-    if (nrows < 0):
-        raise ValueError("nrows can't be a negative number")
-    
-    if (num_cols < 0):
-        raise ValueError("num_cols can't be a negative number")
-    
-    if (string_cols < 0):
-        raise ValueError("string_cols can't be a negative number")
-    
-    if (cat_cols < 0):
-        raise ValueError("cat_cols can't be a negative number")
-    
-    if (time_cols < 0):
-        raise ValueError("time_cols can't be a negative number")
-    
-    if (uuid_cols < 0):
-        raise ValueError("uuid_cols can't be a negative number")
-    
-    BASE_MEM_REQUIREMENT_MB = 32
-    SAFETY_FACTOR = 4
-    BYTES_IN_MB = 1024 * 1024
-    BYTES_IN_GB = 1024 * BYTES_IN_MB
-
-    known_cols = num_cols + string_cols + uuid_cols + cat_cols + time_cols
-    
-    if (known_cols > ncols):
-        raise ValueError("There can not be more specific columns then columns in total")
-
-    unknown_cols = ncols - known_cols
-    unknown_size = 8
-    unknown_requirement = unknown_cols * nrows * unknown_size
-    num_size = 8
-    num_requirement = num_cols * nrows * num_size
-    string_size = 128
-    string_requirement = string_size * string_cols * nrows
-    uuid_size = 16
-    uuid_requirement = uuid_size * uuid_cols * nrows
-    cat_size = 2
-    cat_requirement = cat_size * cat_cols * nrows
-    time_size = 8
-    time_requirement = time_size * time_cols * nrows
-    data_requirement = unknown_requirement + num_requirement + string_requirement + uuid_requirement + cat_requirement + time_requirement
-    mem_req = (BASE_MEM_REQUIREMENT_MB * BYTES_IN_MB + data_requirement) * SAFETY_FACTOR / BYTES_IN_GB
-    return math.ceil(mem_req)
 
 #-----------------------------------------------------------------------------------------------------------------------
 # Private
