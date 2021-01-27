d72b3da3b1f773a827ea4fde894ff738016fda98
michalkurka <michalk@h2o.ai>
2021-01-27 17:35:55 +0200

    Added JUnit tests
    (cherry picked from commit 602e5209b1b167ed23c78c136ca044301e4608be)

--- h2o-core/src/test/java/water/persist/PersistManagerTest.java
+++ h2o-core/src/test/java/water/persist/PersistManagerTest.java
@@ -5,11 +5,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import water.*;
+import water.fvec.*;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
-import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
@@ -32,7 +32,7 @@ public class PersistManagerTest extends TestUtil {
         List<String> matches = persistManager.calcTypeaheadMatches("", 100);
         assertNotNull(matches);
         assertEquals(0, matches.size());
-        
+
         // Path with spaces (testing trim is being done)
         matches = persistManager.calcTypeaheadMatches("   ", 100);
         assertNotNull(matches);
@@ -49,18 +49,9 @@ public class PersistManagerTest extends TestUtil {
 
     @Test
     public void deltaLogFilesAreFiltered() throws IOException {
-        testDeltaLogFilesAreFiltered(File::getAbsolutePath);
-    }
-
-    @Test
-    public void deltaLogFilesAreFiltered_pubdev() throws IOException {
-        testDeltaLogFilesAreFiltered(dir -> dir.getAbsolutePath() + "/");
-    }
-
-    private void testDeltaLogFilesAreFiltered(Function<File, String> makePath) throws IOException {
         File dir = tmp.newFolder("to_import");
         Files.write(new File(dir, "file-a").toPath(), Collections.singleton("file-a"));
-        Files.write(new File(dir, "file-b").toPath(), Collections.singleton("file-b"));
+        Files.write(new File(dir, "file-b").toPath(), Collections.singleton("file-a"));
         File deltaLogDir = new File(dir, "_delta_log");
         assertTrue(deltaLogDir.mkdir());
         Files.write(new File(deltaLogDir, "000.json").toPath(), Collections.singleton("json data"));
@@ -71,7 +62,7 @@ public class PersistManagerTest extends TestUtil {
         ArrayList<String> fails = new ArrayList<>();
         ArrayList<String> dels = new ArrayList<>();
         try {
-            persistManager.importFiles(makePath.apply(dir), null, files, keys, fails, dels);
+            persistManager.importFiles(dir.getAbsolutePath(), null, files, keys, fails, dels);
 
             assertEquals(2, files.size());
             assertEquals(2, keys.size());
@@ -88,24 +79,65 @@ public class PersistManagerTest extends TestUtil {
             }
         }
     }
-    
+
     @Test
-    public void testDeltaLakeMetadataFilter() {
-        PersistManager.DeltaLakeMetadataFilter filter = new PersistManager.DeltaLakeMetadataFilter();
-        List<String> result = filter.apply(null, new ArrayList<>(Arrays.asList(
-                "hdfs://localhost",
-                "/a/file.parquet",
-                "/a/_delta_log/b/_delta_log/00.crc",
-                "/a/_delta_log/b/fileA.parquet",
-                "dbfs:///_delta_log/00.crc",
-                "dbfs:///_delta_log/b/fileB.parquet"
-        )));
-        assertEquals(Arrays.asList(
-                "hdfs://localhost",
-                "/a/file.parquet",
-                "/a/_delta_log/b/fileA.parquet",
-                "dbfs:///_delta_log/b/fileB.parquet"
-        ), result);
+    public void testIsHexPath() {
+        assertTrue(persistManager.isHexPath("hex://anything"));
+        assertFalse(persistManager.isHexPath("http://anything"));
     }
-    
+
+    @Test
+    public void testToHexPath() {
+        Scope.enter();
+        try {
+            Frame f = TestFrameCatalog.oneChunkFewRows();
+            Key chunkKey = f.anyVec().chunkKey(0);
+            String hexPath = persistManager.toHexPath(chunkKey);
+            assertTrue(hexPath.startsWith("hex://"));
+            assertTrue(persistManager.isHexPath(hexPath));
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void testOpenHexPath() throws IOException {
+        Scope.enter();
+        try {
+            Vec v = Vec.makeConN(10, 1);
+            Scope.track(v);
+            byte[] data = new byte[(int) v.length()];
+            Key chunkKey = v.chunkKey(0);
+            DKV.put(chunkKey, new C1NChunk(data));
+
+            InputStream is = persistManager.open(persistManager.toHexPath(chunkKey));
+            assertTrue(is instanceof ByteArrayInputStream);
+            assertEquals(data.length, is.available());
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void testCreateHexPath() throws IOException {
+        Scope.enter();
+        try {
+            Vec v = Vec.makeConN(10, 1);
+            Key chunkKey = v.chunkKey(0);
+            Scope.track(v);
+
+            byte[] newData = new byte[v.chunkLen(0)];
+            for (int i = 0; i < newData.length; i++)
+                newData[i] = (byte) i;
+            try (OutputStream os = persistManager.create(persistManager.toHexPath(chunkKey), true)) {
+                os.write(newData);
+            }
+            Chunk c = v.chunkForChunkIdx(0);
+            assertTrue(c instanceof C1NChunk);
+            assertArrayEquals(newData, c.asBytes());
+        } finally {
+            Scope.exit();
+        }
+    }
+
 }
